f201a568caf6085a85ab90d18a686cb245a51392
ekkasit <ekkasit.phumiket@gmail.com>
2026-06-29 23:08:47 +0700

    chore(api): remove test-db script

--- apps/api/test-db.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-async function main() {
-  const assets = await prisma.asset.findMany({ take: 5, orderBy: { createdAt: 'desc' } });
-  console.log(JSON.stringify(assets, null, 2));
-}
-main().catch(console.error).finally(() => prisma.$disconnect());
