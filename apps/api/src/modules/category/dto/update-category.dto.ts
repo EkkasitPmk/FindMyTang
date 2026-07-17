538e29800018a6cff83bce9e6f6e762a196861c3
ekkasit <ekkasit.phumiket@gmail.com>
2026-07-18 06:37:32 +0700

    refactor(api): exclude id from category and transaction update DTOs

--- apps/api/src/modules/category/dto/update-category.dto.ts
+++ apps/api/src/modules/category/dto/update-category.dto.ts
@@ -1,4 +1,15 @@
 import { PartialType } from "@nestjs/swagger";
 import { CreateCategoryDto } from "./create-category.dto";
+import { IsOptional } from "class-validator";
+import { CategoryType } from "@prisma/client";
 
-export class UpdateCategoryDto extends PartialType(CreateCategoryDto) {}
+export class UpdateCategoryDto extends PartialType(CreateCategoryDto) {
+  @IsOptional()
+  name?: string;
+  @IsOptional()
+  icon?: string;
+  @IsOptional()
+  color?: string;
+  @IsOptional()
+  type?: CategoryType;
+}
--- apps/api/src/modules/transaction/dto/update-transaction.dto.ts
+++ apps/api/src/modules/transaction/dto/update-transaction.dto.ts
@@ -2,8 +2,26 @@ import { PartialType } from "@nestjs/swagger";
 import { CreateTransactionDto } from "./create-transaction.dto";
 import { IsOptional } from "class-validator";
 import { Transform } from "class-transformer";
+import { TransactionType } from "@prisma/client";
 
 export class UpdateTransactionDto extends PartialType(CreateTransactionDto) {
+  @IsOptional()
+  type?: TransactionType;
+  @IsOptional()
+  amount?: number;
+  @IsOptional()
+  date?: string;
+  @IsOptional()
+  categoryId?: string;
+  @IsOptional()
+  assetId?: string;
+  @IsOptional()
+  toAssetId?: string;
+  @IsOptional()
+  note?: string;
+  @IsOptional()
+  attachmentUrl?: string;
+
   @IsOptional()
   @Transform(({ value }) =>
     value === null || value === "null" || value === ""
