b7ea87f20ca81b02c6242f61ff0aba495f4a8dc3
ekkasit <ekkasit.phumiket@gmail.com>
2026-07-30 04:35:48 +0700

    build(deps): update project dependencies and package lockfile

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -10,7 +10,6 @@ const nextConfig: NextConfig = {
     root: path.resolve(__dirname, "../../"),
   },
   reactCompiler: true,
-  allowedDevOrigins: ["192.168.1.106"],
   images: {
     remotePatterns: [
       {
