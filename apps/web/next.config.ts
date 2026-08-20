a26833f9ad311a671a93484721d64c5842333f5e
ekkasit <ekkasit.phumiket@gmail.com>
2026-08-20 18:46:26 +0700

    chore(web): update allowed dev origin in next config

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -7,7 +7,7 @@ const BACKEND_URL =
 
 const devConfig: Partial<NextConfig> =
   process.env.NODE_ENV === "development"
-    ? { allowedDevOrigins: ["192.168.1.148"] }
+    ? { allowedDevOrigins: ["192.168.1.149"] }
     : {};
 
 const nextConfig: NextConfig = {
